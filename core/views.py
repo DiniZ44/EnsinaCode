6d881140d5d2c7300fca486b72ea1218d40d4305
Hakkinen Diniz <santoshakkinen7@gmail.com>
2021-01-28 15:37:18 -0300

    Updates, login and register With Feedback OK

--- core/views.py
+++ core/views.py
@@ -32,9 +32,15 @@ def reg_done(request):
     reg_usuario.email = email_rec
     reg_usuario.senha = senha_rec
 
+    if  nome_rec == ""  or senha_rec=="" or email_rec=='' or senha_rec1=="":
+        context = {'msg': 'Existem campos obrigatorios vazios'}
+        return render(request, 'core/register.html', context)
+
     if senha_rec1 == senha_rec :
         reg_usuario.save()
         return render(request, 'core/reg_done.html', )
+
+
 
     else:
         context = {'msg': 'As senhas diferem'}
@@ -45,6 +51,11 @@ def login_done(request):
 
         if Usuario.objects.filter(email= request.GET.get('email_login') ,senha=request.GET.get('senha_login')).exists() :
             return render(request, 'core/index.html',)
+
+        if request.GET.get('email_login') == "" or request.GET.get('senha_login') == "":
+            context = {'msg': 'Email ou Senha vazio'}
+            return render(request, 'core/login.html', context)
+
         else:
             context = {'msg': 'Email ou senha incorretos'}
             return render(request, 'core/login.html', context)
