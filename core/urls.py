3dc23ab34844831c30083330099ebdb0346df0da
Hakkinen Diniz <santoshakkinen7@gmail.com>
2021-01-28 10:36:26 -0300

    Updates, create views index, login, register, questions and ranking

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.Index, name='Index.html')
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-
-def Index(request):
+def index(request):
     return render(request, 'core/index.html')
+
+def register(request):
+    return  render(request, 'core/register.html')
+
+def login(request):
+    return  render(request, 'core/login.html')
+
+def linguagens(request):
+    return  render(request, 'core/linguagens.html')
+
+def ranking(request):
+    return  render(request, 'core/ranking.html')
+
+def questions(request):
+    return  render(request, 'core/questions.html')
